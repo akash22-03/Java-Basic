d4475d3834221838956b89b0c6ae7d64c334f8ed
AKASH DASH <56639516+akash22-03@users.noreply.github.com>
2021-05-12 08:48:36 +0530

    Create LeapYear.java

---
+++ Control Structures/LeapYear.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LeapYear{
+    
+    public static void main(String[] args){
+        
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter the Year");
+       int year = sc.nextInt();
+       
+        if (year>999 && year<10000)
+       {
+        
+        if(year%4==0&&year%100!=0||year%400==0)
+           System.out.println("Leap Year");
+           
+       else 
+           System.out.println("Not a Leap Year");
+       }
+       else
+           System.out.println("Invalid Year");
+       }
+       
+   }
